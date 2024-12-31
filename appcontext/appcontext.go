8f14c8224639ca6020320a1e7e21a73b47175dcf
Nam HQ <namhq.1989@gmail.com>
2024-12-31 22:22:41 +0700

    fix(appcontext): set traceId to logger

--- appcontext/appcontext.go
+++ appcontext/appcontext.go
@@ -56,6 +56,7 @@ func NewWorker(ctx context.Context) *AppContext {
 
 func (appCtx *AppContext) SetTraceID(traceID string) {
 	appCtx.traceID = traceID
+	appCtx.logger.AddData(logger.Fields{"traceId": traceID})
 }
 
 func (appCtx *AppContext) GetTraceID() string {
