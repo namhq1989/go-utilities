9552f4117892a1ea48439357860fe86172d4fe76
Nam HQ <namhq.1989@gmail.com>
2026-07-09 13:45:30 +0700

    feat(language): add Japanese, Korean, Chinese, Thai language support
    Add 4 new language constants (ja, ko, zh, th) with IsXxx() methods and
    ToLanguage() mapping. Previously ToLanguage returned Unknown for these
    codes, causing appcontext.GetLang() to fall back to Vietnamese when an
    app-server client sent Accept-Language: ja/ko/zh/th.

--- go.mod
+++ go.mod
@@ -11,5 +11,5 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/stretchr/testify v1.11.1 // indirect
-	golang.org/x/sys v0.43.0 // indirect
+	golang.org/x/sys v0.45.0 // indirect
 )
--- language/language.go
+++ language/language.go
@@ -6,6 +6,10 @@ const (
 	Unknown    Language = ""
 	English    Language = "en"
 	Vietnamese Language = "vi"
+	Japanese   Language = "ja"
+	Korean     Language = "ko"
+	Chinese    Language = "zh"
+	Thai       Language = "th"
 )
 
 func (l Language) String() string {
@@ -24,6 +28,22 @@ func (l Language) IsVietnamese() bool {
 	return l == Vietnamese
 }
 
+func (l Language) IsJapanese() bool {
+	return l == Japanese
+}
+
+func (l Language) IsKorean() bool {
+	return l == Korean
+}
+
+func (l Language) IsChinese() bool {
+	return l == Chinese
+}
+
+func (l Language) IsThai() bool {
+	return l == Thai
+}
+
 func (l Language) GetCountry() string {
 	switch l {
 	case Vietnamese:
@@ -39,6 +59,14 @@ func ToLanguage(lang string) Language {
 		return English
 	case Vietnamese.String():
 		return Vietnamese
+	case Japanese.String():
+		return Japanese
+	case Korean.String():
+		return Korean
+	case Chinese.String():
+		return Chinese
+	case Thai.String():
+		return Thai
 	default:
 		return Unknown
 	}
